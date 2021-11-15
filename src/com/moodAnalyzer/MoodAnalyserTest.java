f1198687a64722741ee7322053748000cf9277a5
pitu <ashishkumarvish.432@gmail.com>
2021-11-15 15:37:01 +0530

    Happy mood

--- src/com/moodAnalyzer/MoodAnalyserTest.java
+++ src/com/moodAnalyzer/MoodAnalyserTest.java
@@ -3,26 +3,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-    MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-
-    // Method to test if the mood is sad
     @Test
-    public void whenTestedMood_ifSad_shouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+    public void iAmin_SadMood_ShouldReturn_sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("i am in in sad mood");
         String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("sad", mood);
-
+        Assert.assertEquals("sad",mood);
     }
-    // Method to test if the mood is
-    @Test
-    public void whenTestedMood_ifHappy_shouldReturnHappy(){
-        String mood = moodAnalyzer.analyseMood("Happy Days");
-        Assert.assertEquals("Happy" , mood);
-
-    }
-    @Test
-    public void  whenTestedMood_ifNotIMood_shouldReturnHappy(){
-        String mood = moodAnalyzer.analyseMood("i am not in a mood");
+    public void iAmIn_anyMood_ShouldReturn_happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("i am any mood");
+        String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals("Happy", mood);
     }
 }
--- src/com/moodAnalyzer/MoodAnalyzer.java
+++ src/com/moodAnalyzer/MoodAnalyzer.java
@@ -1,21 +1,29 @@
 package com.moodAnalyzer;
 
 public class MoodAnalyzer {
+    private String message;
+    private String sad;
 
 
-    public static String analyseMood(String message) {
+    public MoodAnalyzer(String message) {
+        this.message = message;
+
+    }
+    public String analyseMood(String message) {
+
         if (message.contains("sad"))
             return "sad";
         else
             return "Happy";
-
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser");
-        String message = "I am sad";
-        System.out.println(analyseMood(message));
+
     }
 
 
 
+    public String analyseMood() {
+        return sad;
+    }
 }
